3e99ca9944b574ba9e8da7563c94da5a4e30c93f
@adrian.istrefi.nuppnau <adrian.istrefi.nuppnau@lego.com>
2023-11-22 10:25:26 +0100

    Make required fields for creating and updating a vinyl

--- Shared/DTOs/VinylDto.cs
+++ Shared/DTOs/VinylDto.cs
@@ -1,11 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.DTOs;
 
 public class VinylDto
 {
     public int VinylId { get; set; }
+    [Required]
     public string Title { get; set; }
+    [Required]
     public string? Artist { get; set; }
+    [Required]
     public decimal Price { get; set; }
+    [Required]
     public string ImagePath { get; set; }
     public List<GenreDto> Genres { get; set; }
 }
